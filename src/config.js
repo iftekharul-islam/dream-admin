ac08f7d6bf57b32fa167034d3ce13dc01ea1e7f4
imrul18 <imrul.afnan18@gmail.com>
2023-06-21 23:44:28 +0600

    build

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // const baseUrl = 'http://dream-records.test/api/admin'
-const baseUrl = 'https://api-dream-records.imrul.xyz/api/admin'
+const baseUrl = 'https://api-dream-records.viserx.dev/api/admin'
 
 export default baseUrl
